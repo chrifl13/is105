eca682a32dcbc284f3060198e5c7dadbfab0ee94
chifl13@gmail.com <chrifl13@gmail.com>
2014-04-02 06:55:58 +0200

    andre

---
+++ lab/python/exercises/ex5.py
@@ -0,0 +1,17 @@
+my_name = 'zed a shaw'
+my_age = 35 # fag
+my_height = 180 # cm
+my_weight = 50 # kg
+my_eyes = 'blue' 
+my_teeth = 'brown'
+my_hair = 'brown'
+
+print "Lets talk about %s." % my_name
+print "hes %d cm tall." % my_height
+print "hes %d kg heavy." % my_weight
+print "actually thats not too heavy."
+print "hes gpt %s eyes and %s hair." % (my_eyes, my_hair)
+print "his teeth are usually %s depending on the coffee." % my_teeth
+
+# this line is tricky, try to get it exactly right
+print "if i add %d, %dm and %d I get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight)
